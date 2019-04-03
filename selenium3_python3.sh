aa4388fa72483088a33930c80ecfc3be03b8f945
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-04-03 16:11:23 -0400

    Update selenium3_python3.sh

--- selenium3_python3.sh
+++ selenium3_python3.sh
@@ -4,6 +4,8 @@ sudo apt-get update -y
 sudo apt-get -y upgrade -y
 python3 -V
 sudo apt-get install python3-pip -y
+pip3 --version 
+pip3 install --upgrade pip
 sudo python3 -m pip install -U selenium
 sudo apt-get install build-essential libssl-dev libffi-dev python3-dev 
 sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
