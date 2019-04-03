b5c9ae6f60314a18bb9dc33c376169ffecce7e87
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-04-04 01:21:02 +0530

    Create selenium3_python3.sh

---
+++ selenium3_python3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get -y upgrade -y
+python3 -V
+sudo apt-get install python3-pip -y
+sudo python3 -m pip install -U selenium
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev 
+sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+sudo tar -xzvf geckodriver-v0.24.0-linux64.tar.gz
+sudo chmod +x geckodriver
+sudo cp -r geckodriver /usr/local/bin/
+
+
+nano anyname.py
+
+#!/usr/bin/env python
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://www.ubuntu.com/')
