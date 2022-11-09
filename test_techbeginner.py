59f0a5e5e05e36b149c1af25d1a6dfcf115e6122
aftab70 <43289912+aftab70@users.noreply.github.com>
2022-11-09 14:33:32 +0530

    Update test_techbeginner.py

--- test_techbeginner.py
+++ test_techbeginner.py
@@ -4,10 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
  
-#options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--no-sandbox')
-#options.add_argument('--disable-dev-shm-usage')
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
  
 def test():
