bae4b913a70b13abc767f8ffa6d99920a96b65cb
aftab70 <43289912+aftab70@users.noreply.github.com>
2023-01-15 01:02:39 +0530

    Create selenium_setup.sh

---
+++ selenium_setup.sh
@@ -0,0 +1,30 @@
+sudo apt-get update 
+wget -nc https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+sudo apt-get update 
+sudo apt install -f ./google-chrome-stable_current_amd64.deb
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.8
+python --version
+python3 --version
+pip3 install selenium webdriver-manager 
+sudo apt-get install python3-pip
+pip3 install selenium webdriver-manager 
+nano test_kalillinux.py
+python3 test_kalillinux.py 
+pip3 install urllib3
+python3 test_kalillinux.py 
+nano test_kalillinux.py 
+python3 test_kalillinux.py 
+pip3 install pytest 
+python3 test_kalillinux.py 
+python3 -m pytest test_kalillinux.py 
+pip3 install requests
+python3 -m pytest test_kalillinux.py 
+pip install requests
+python3 -m pytest test_kalillinux.py 
+python3  test_kalillinux.py 
+pip3 install --upgrade requests
+python3  test_kalillinux.py 
